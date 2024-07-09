9701a5b87cd2fd57a4694dc77c7eeee00656b2b8
DavidingPlus <2065666169@qq.com>
2024-07-09 16:17:11 +0800

    Revert "add tips for customers"
    This reverts commit d941a9ab4f3bce6ab8dd2ed0bb54ed9b901b7a1e.

--- src/mycircle.cpp
+++ src/mycircle.cpp
@@ -6,19 +6,7 @@ void MyCircle::paint(LDrawContext *dc)
     switch (m_state)
     {
         case PieceState::UnActivated:
-        {
-            dc->setBrushColor(LColor(0xffffff));
-            dc->fillCircle(LCircle(m_roundCenterX, m_roundCenterY, m_roundRadius));
-
             break;
-        }
-        case PieceState::ReadyToActivate:
-        {
-            dc->setBrushColor(LColor(0xffff00));
-            dc->fillCircle(LCircle(m_roundCenterX, m_roundCenterY, m_roundRadius - 20));
-
-            break;
-        }
         case PieceState::Head:
         {
             dc->setBrushColor(LColor(0xff0000));
